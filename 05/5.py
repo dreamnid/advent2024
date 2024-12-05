aafd18132b0d872586ffd4108c8ad741bf5bcb60
Giovanni Go <dreamnid@dreamnid.com>
2024-12-04 22:50:17 -0800

    Part 1 of day 5

---
+++ 05/5.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+from collections import Counter, defaultdict, deque
+from collections.abc import Callable, Collection, Iterable, Sequence
+from dataclasses import dataclass
+from functools import partial, reduce
+from itertools import chain, cycle, takewhile
+import math
+from operator import mul, ge, gt, itemgetter, le, lt
+import os
+import pprint
+import re
+from time import time
+from typing import NamedTuple
+
+from humanize import intcomma
+import numpy as np
+import pyparsing as pp
+import pandas as pd
+
+# Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
+if __name__ == '__main__':
+    if not __package__:
+        import sys
+        from os import path
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+        # Relative imports here
+        from util import *
+
+INPUT_FILE='5-input.txt'
+# INPUT_FILE='5a-example.txt'
+
+rules = [[int(char) for char in line.split('|')] for line in get_file_contents(INPUT_FILE)[0]]
+updates = [[int(char) for char in line.split(',')] for line in get_file_contents(INPUT_FILE)[1]]
+
+def check_rules(pages: list[int]):
+    for rule in rules:
+        try:
+            first_pos = pages.index(rule[0])
+            last_pos = pages.index(rule[1])
+            if first_pos > last_pos:
+                return False
+        except ValueError:
+            pass
+    return True
+
+
+def get_middle_el(my_list: list):
+    idx = (len(my_list) - 1)//2
+    return my_list[idx]
+
+
+middle_filtered = [get_middle_el(update) for update in updates if check_rules(update)]
+print('a:', sum(middle_filtered))
