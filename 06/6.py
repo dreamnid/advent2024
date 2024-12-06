a4018f801aeaef13fb8f43bb22b9ddf8e44e3f56
Giovanni Go <dreamnid@dreamnid.com>
2024-12-05 22:51:06 -0800

    Part 2 for Day 6

--- 06/6.py
+++ 06/6.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from collections import Counter, defaultdict, deque
 from collections.abc import Callable, Collection, Iterable, Sequence
+from copy import deepcopy
 from dataclasses import dataclass
 from enum import Enum
 from functools import partial, reduce
@@ -52,40 +53,66 @@ def get_dir_addr(my_dir: Dir):
     return None
 
 input = add_padding([[char for char in line] for line in get_file_contents(INPUT_FILE)[0]], '@')
+orig_pos = None
 cur_pos = None
+
 cur_dir = None
 for row_i, row in enumerate(input):
     for col_i, col in enumerate(row):
         if col not in ['.', '#', '@']:
-            cur_pos = (row_i, col_i)
-            cur_dir = Dir(col)
-            cur_val = col
-            input[row_i][col_i] = 'X'
+            cur_pos = orig_pos = (row_i, col_i)
+            cur_dir = orig_dir = Dir(col)
+            cur_val = orig_val = col
             break
 
 print(cur_dir, cur_pos)
 
-num_pos = 1
-dir_vals = list(Dir)
-while cur_val != '@':
-    next_pos_addr = get_dir_addr(cur_dir)
-    next_pos = cur_pos[0] + next_pos_addr[0], cur_pos[1] + next_pos_addr[1]
-    next_char = input[next_pos[0]][next_pos[1]]
-    match next_char:
-        case '.':
-            num_pos += 1
-            cur_char = next_char
-            cur_pos = next_pos
-            input[next_pos[0]][next_pos[1]] = 'X'
-        case 'X':
-            cur_char = next_char
-            cur_pos = next_pos
-        case '#':
-            cur_dir = dir_vals[(dir_vals.index(cur_dir) + 1) % 4]
-            # print('new dir', cur_dir)
-        case '@':
-            cur_char = next_char
-            break
-    # pprint.pprint(input)
+def solver(cur_input: list[list[str]], cur_pos, cur_val: str, cur_dir: Dir):
+    visited = defaultdict(set)
+    """pos -> list of dir"""
 
-print('a:', num_pos)
+    num_pos = 1
+    dir_vals = list(Dir)
+    while cur_val != '@':
+        next_pos_addr = get_dir_addr(cur_dir)
+        next_pos = cur_pos[0] + next_pos_addr[0], cur_pos[1] + next_pos_addr[1]
+        next_char = cur_input[next_pos[0]][next_pos[1]]
+        match next_char:
+            case '.':
+                num_pos += 1
+                cur_char = next_char
+                cur_pos = next_pos
+                cur_input[next_pos[0]][next_pos[1]] = 'X'
+            case 'X':
+                visited[cur_pos].add(cur_dir)
+                cur_char = next_char
+                cur_pos = next_pos
+            case '#':
+                cur_dir = dir_vals[(dir_vals.index(cur_dir) + 1) % 4]
+                # print('new dir', cur_dir)
+            case '@':
+                cur_char = next_char
+                break
+            case _:
+                # starting pos
+                cur_char = next_char
+                cur_pos = next_pos
+        if cur_pos in visited and cur_dir in visited[cur_pos]:
+            return None
+    return num_pos
+
+with PrintTiming('a'):
+    print('a:', solver(input, cur_pos, cur_val, cur_dir))
+
+with PrintTiming('b'):
+    num_loops = 0
+    for row_i, row in enumerate(input):
+        for col_i, col in enumerate(row):
+            if col == 'X':
+                # Make a copy and make current position an obstable
+                new_input = deepcopy(input)
+                new_input[row_i][col_i] = '#'
+                if solver(new_input, orig_pos, orig_val, orig_dir) is None:
+                    num_loops += 1
+
+print('b:', num_loops)
---
+++ 06/README.md
@@ -0,0 +1,14 @@
+# Day 6
+
+If have time, explore some algorithm that takes the position of the obstacles and try to form rectangles. Is a pain because need to make sure
+that it does intersects with the original path and need to offset the obstacles position by 1
+
+## Performance
+```bash
+(adventofcode) adventofcode➜  06 git:(main) ✗ python 6.py
+Dir.UP (50, 40)
+a: 5305
+timer a:  0:00:00.001647
+timer b:  0:00:25.480271
+b: 2143
+```
