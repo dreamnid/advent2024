3fc9904031d068054fad237b3bba92d536f69a63
Giovanni Go <dreamnid@dreamnid.com>
2024-12-09 01:12:35 -0600

    Part 2 of day 9

--- 09/9.py
+++ 09/9.py
@@ -33,20 +33,52 @@ INPUT_FILE='9-input.txt'
 
 input = [int(cur_char) for line in get_file_contents(INPUT_FILE)[0] for cur_char in line]
 
+
+def get_continuous_size(block_id):
+    cur_size = 0
+    target_val = disk[block_id]
+    for i in range(block_id, len(disk) - 1):
+        if disk[i] == target_val:
+            cur_size += 1
+        else:
+            break
+
+    return cur_size
+
 # print(input)
 
 # populate disk
-disk = []
+input_disk = []
 cur_id = 0
+cur_block = 0
+file_size = {}
+file_first_block = {}
+files = []  # file id, size
+temp_free_space = []  # blk id, space
+free_space = []  # blk id, space
+
 for i, cur_num in enumerate(input):
     if i % 2 == 0:
-        disk.extend([cur_id] * cur_num)
+        input_disk.extend([cur_id] * cur_num)
+        file_size[cur_id] = cur_num
+        file_first_block[cur_id] = cur_block
+        files.append((cur_id, cur_num))
         cur_id += 1
     else:
-        disk.extend([None] * cur_num)
-        pass
+        if cur_num:
+            input_disk.extend([None] * cur_num)
+            temp_free_space.append((cur_block, cur_num))
+            free_space.append((cur_block, cur_num))
+    cur_block += cur_num
+
+# for cur_blk_id, cur_free_space in temp_free_space:
+#     if not free_space or (free_space and cur_blk_id > (free_space[-1][0] + free_space[-1][1])):
+#         size = get_continuous_size(cur_blk_id)
+#         free_space.append((cur_blk_id, size))
+# print(disk)
 
 with PrintTiming('a'):
+    disk = input_disk.copy()
     for i, cur_num in enumerate(reversed(disk)):
         real_idx = len(disk) - 1 - i
         if cur_num is not None:
@@ -64,3 +96,32 @@ with PrintTiming('a'):
 
     chksum = [i * val for i, val in enumerate(disk) if val is not None]
 print('a:', sum(chksum))
+
+with PrintTiming('b'):
+    disk = input_disk.copy()
+    unmoved_files = list(reversed(files))
+    for unmoved_file_id, cur_size in unmoved_files:
+        for space_idx, (space_blk_id, space_size) in enumerate(free_space):
+            if space_blk_id < file_first_block[unmoved_file_id] and cur_size <= space_size:
+                for k in range(space_blk_id, space_blk_id + cur_size):
+                    disk[k] = unmoved_file_id
+                for k in range(file_first_block[unmoved_file_id], file_first_block[unmoved_file_id] + cur_size):
+                    disk[k] = None
+
+                if cur_size == space_size:
+                    del free_space[space_idx]
+                else:
+                    # Calc new space left
+                    free_space[space_idx] = (space_blk_id + cur_size, space_size - cur_size)
+                break
+
+        # print(disk)
+        # print(free_space)
+    # move free space from beginning to end
+    # for i in range(disk.index(0)):
+    #     del disk[0]
+    #     disk.append(None)
+
+    chksum = [i * val for i, val in enumerate(disk) if val is not None]
+# print(disk)
+print('b:', sum(chksum))
---
+++ 09/README.md
@@ -0,0 +1,14 @@
+# Day 9
+
+### Part 2 attempts
+
+* 8446022420670 -- too high
+* 15509274113003 -- too high
+
+## Performance
+```bash
+(advent2022) ~/w/a/09 ❯❯❯ ./9.py                                                                                                                                                                                                                   main ⬆ ✱ ◼
+timer a:  0:00:26.800694
+a: 6225730762521
+timer b:  0:00:04.130606
+```
