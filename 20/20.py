56ab45842e2bb12083bc347acc7f665666afaf4a
Giovanni Go <dreamnid@dreamnid.com>
2024-12-20 02:50:12 -0600

    Part 2 of day 20 (incorrect answer)

--- 20/20.py
+++ 20/20.py
@@ -19,6 +19,7 @@ if __name__ == '__main__':
 INPUT_FILE='20-input.txt'
 # INPUT_FILE='20a-example.txt'
 
+
 class Dir(IntEnum):
     RIGHT = 0
     DOWN = 1
@@ -31,31 +32,45 @@ class Pos(NamedTuple):
     col: int
 
 
-def get_neighbors(input, pos: Pos):
+def get_neighbors(input, pos: Pos, finish_pos=None):
     res = []
 
-    if input[pos.row-1][pos.col] != '#':
+    if input[pos.row-1][pos.col] in ['.', 'E', 'S']:
         res.append((Pos(pos.row-1, pos.col)))
-    if input[pos.row][pos.col+1] != '#':
+    if input[pos.row][pos.col+1] in ['.', 'E', 'S']:
         res.append((Pos(pos.row, pos.col+1)))
-    if input[pos.row+1][pos.col] != '#':
+    if input[pos.row+1][pos.col] in ['.', 'E', 'S']:
         res.append((Pos(pos.row+1, pos.col)))
-    if input[pos.row][pos.col-1] != '#':
+    if input[pos.row][pos.col-1] in ['.', 'E', 'S']:
         res.append((Pos(pos.row, pos.col-1)))
 
     return res
 
-def get_wall_neighbors(input, pos: Pos):
+def get_wall_neighbors_with_dir(input, pos: Pos, finish_pos=None):
     res = []
 
-    if input[pos.row-1][pos.col] == '#':
+    if input[pos.row-1][pos.col] == '#' or (finish_pos == Pos(pos.row-1, pos.col)):
         res.append((Pos(pos.row-1, pos.col), Dir.UP))
-    if input[pos.row][pos.col+1] == '#':
+    if input[pos.row][pos.col+1] == '#' or (finish_pos == Pos(pos.row, pos.col+1)):
         res.append((Pos(pos.row, pos.col+1), Dir.RIGHT))
-    if input[pos.row+1][pos.col] == '#':
+    if input[pos.row+1][pos.col] == '#' or (finish_pos == Pos(pos.row+1, pos.col)):
         res.append((Pos(pos.row+1, pos.col), Dir.DOWN))
-    if input[pos.row][pos.col-1] == '#':
+    if input[pos.row][pos.col-1] == '#' or (finish_pos == Pos(pos.row, pos.col-1)):
         res.append((Pos(pos.row, pos.col-1), Dir.LEFT))
+    return res
+
+
+def get_cheat_neighbors(input, pos: Pos, finish_pos=None):
+    res = []
+
+    if input[pos.row-1][pos.col] != '/':
+        res.append(Pos(pos.row-1, pos.col))
+    if input[pos.row][pos.col+1] != '/':
+        res.append(Pos(pos.row, pos.col+1))
+    if input[pos.row+1][pos.col] != '/':
+        res.append(Pos(pos.row+1, pos.col))
+    if input[pos.row][pos.col-1] != '/':
+        res.append(Pos(pos.row, pos.col-1))
     return res
 
 
@@ -80,7 +95,7 @@ def manhattan_distance(a: Pos, b: Pos):
     return abs(a.row-b.row) + abs(a.col-b.col)
 
 
-def a_star(input, start: Pos, end: Pos):
+def a_star(input, start: Pos, end: Pos, neighbors_func=get_neighbors):
     myqueue = PriorityQueue()
     myqueue.put(start, 0)
 
@@ -97,7 +112,7 @@ def a_star(input, start: Pos, end: Pos):
         if current_pos == end:
             break
 
-        for next in get_neighbors(input, current_pos):
+        for next in neighbors_func(input, current_pos, end):
             new_cost = cost[current_pos] + 1
             if next not in cost or new_cost < cost[next]:
                 cost[next] = new_cost
@@ -107,7 +122,14 @@ def a_star(input, start: Pos, end: Pos):
 
     return came_from, cost
 
-input = add_padding([[cur_char for cur_char in line] for line in get_file_contents(INPUT_FILE)[0]], '#')
+# Change border to another character
+file_contents = get_file_contents(INPUT_FILE)[0]
+file_contents[0] = '/' * len(file_contents[0])
+file_contents[len(file_contents)-1] = '/' * len(file_contents[0])
+
+input = [['/' if i == 0 or i == len(file_contents[0]) - 1 else cur_char for i, cur_char in enumerate(line)] for line in file_contents]
+# pprint.pprint(input)
+
 
 start_pos = None
 end_pos = None
@@ -128,47 +150,83 @@ class Axis(IntEnum):
     Y = 1
 
 savings = defaultdict(int)
-with PrintTiming('a'):
-    new_path_nodes, cost = a_star(input, start_pos, end_pos)
-    original_cost = len(new_path_nodes) - 1
-    print(cost[end_pos])
-    walls_tried = set()
+# with PrintTiming('a'):
+#     new_path_nodes, cost = a_star(input, start_pos, end_pos)
+#     original_cost = len(new_path_nodes) - 1
+#     print(cost[end_pos])
+#     walls_tried = set()
+#
+#     for node in new_path_nodes.keys():
+#         walls = get_wall_neighbors_with_dir(input, node)
+#         next_walls_to_try = []
+#         for wall, dir in walls:
+#             match dir:
+#                 case Dir.UP:
+#                     if input[wall.row-1][wall.col] != '#' and (wall, Axis.Y) not in walls_tried:
+#                         next_walls_to_try.append((wall, Pos(wall.row-1, wall.col)))
+#                         walls_tried.add((wall, Axis.Y))
+#                 case Dir.RIGHT:
+#                     if input[wall.row][wall.col+1] != '#' and (wall, Axis.X) not in walls_tried:
+#                         next_walls_to_try.append((wall, Pos(wall.row, wall.col+1)))
+#                     walls_tried.add((wall, Axis.X))
+#                 case Dir.DOWN:
+#                     if input[wall.row+1][wall.col] != '#' and (wall, Axis.Y) not in walls_tried:
+#                         next_walls_to_try.append((wall, Pos(wall.row+1, wall.col)))
+#                         walls_tried.add((wall, Axis.Y))
+#                 case Dir.LEFT:
+#                     if input[wall.row][wall.col-1] != '#' and (wall, Axis.X) not in walls_tried:
+#                         next_walls_to_try.append((wall, Pos(wall.row, wall.col-1)))
+#                         walls_tried.add((wall, Axis.X))
+#
+#         for next_wall, next_track in next_walls_to_try:
+#             if next_track in cost:
+#                 # print(next_wall, 'cached')
+#                 savings[cost[next_track] - cost[node] - 2] += 1
+#             else:
+#                 # print(next_wall, 'not cached')
+#
+#                 new_input = deepcopy(input)
+#                 new_input[next_wall.row][next_wall.col] = '.'
+#
+#                 new_path_nodes, new_cost = a_star(new_input, start_pos, end_pos)
+#                 new_len = new_cost[end_pos]
+#                 savings[original_cost - new_len] += 1
+#
+# # pprint.pprint(savings)
+# print('a', sum([num for saving, num in savings.items() if saving >= 100]))
+savings_path = defaultdict(list)
 
-    for node in new_path_nodes.keys():
-        walls = get_wall_neighbors(input, node)
-        next_walls_to_try = []
-        for wall, dir in walls:
-            match dir:
-                case Dir.UP:
-                    if input[wall.row-1][wall.col] != '#' and (wall, Axis.Y) not in walls_tried:
-                        next_walls_to_try.append((wall, Pos(wall.row-1, wall.col)))
-                        walls_tried.add((wall, Axis.Y))
-                case Dir.RIGHT:
-                    if input[wall.row][wall.col+1] != '#' and (wall, Axis.X) not in walls_tried:
-                        next_walls_to_try.append((wall, Pos(wall.row, wall.col+1)))
-                    walls_tried.add((wall, Axis.X))
-                case Dir.DOWN:
-                    if input[wall.row+1][wall.col] != '#' and (wall, Axis.Y) not in walls_tried:
-                        next_walls_to_try.append((wall, Pos(wall.row+1, wall.col)))
-                        walls_tried.add((wall, Axis.Y))
-                case Dir.LEFT:
-                    if input[wall.row][wall.col-1] != '#' and (wall, Axis.X) not in walls_tried:
-                        next_walls_to_try.append((wall, Pos(wall.row, wall.col-1)))
-                        walls_tried.add((wall, Axis.X))
+with PrintTiming('b'):
+    max_cheat_time_allowed = 20
+    new_path, cost = a_star(input, start_pos, end_pos)
+    original_cost = cost[end_pos]
 
-        for next_wall, next_track in next_walls_to_try:
-            if next_track in cost:
-                # print(next_wall, 'cached')
-                savings[cost[next_track] - cost[node] - 2] += 1
+    new_path_nodes = list(new_path.keys())
+    nodes_len = len(new_path_nodes)
+    for i, node in enumerate(new_path_nodes):
+        if i % 1000 == 0:
+            print(f'{i}/{nodes_len}', 'node', node)
+        near_nodes = [near_node for near_node in new_path_nodes[new_path_nodes.index(node)+100:] if manhattan_distance(near_node, node) < max_cheat_time_allowed + 1 and near_node not in get_neighbors(input, node) and cost[node] < cost[near_node]]
+
+        for near_node in near_nodes:
+            try:
+                cheat_path_nodes, cheat_cost = a_star(input, node, near_node, neighbors_func=get_cheat_neighbors)
+            except IndexError:
+                pass
             else:
-                # print(next_wall, 'not cached')
+                if cost[node] + cheat_cost[near_node] < cost[near_node] and cheat_cost[near_node] < max_cheat_time_allowed:
+                    savings[cost[near_node] - cost[node] - cheat_cost[near_node]] += 1
+                    savings_path[cost[near_node] - cost[node] - cheat_cost[near_node]].append(cheat_path_nodes)
 
-                new_input = deepcopy(input)
-                new_input[next_wall.row][next_wall.col] = '.'
+        print(f'{sum([num for saving, num in savings.items() if saving >= 100]):,}')
 
-                new_path_nodes, new_cost = a_star(new_input, start_pos, end_pos)
-                new_len = new_cost[end_pos]
-                savings[original_cost - new_len] += 1
+pprint.pprint(savings)
+#
+# savings_path_to_print = savings_path[76][2]
+# for row_i, row in enumerate(input):
+#     for col_i, col in enumerate(row):
+#         char_to_print = 'S' if Pos(row_i, col_i) == start_pos else 'M' if savings_path_to_print and Pos(row_i, col_i) in savings_path_to_print else col
+#         print(char_to_print, end='')
+#     print()
 
-# pprint.pprint(savings)
-print('a', sum([num for saving, num in savings.items() if saving >= 100]))
+print('b', sum([num for saving, num in savings.items() if saving >= 100]))
---
+++ 20/README.md
@@ -0,0 +1,18 @@
+# Day 20
+
+The description talking about the cheat lasting for 2 nanoseconds can be interpreted that you can 
+travel through 2 wall blocks. 
+After careful reading, it really means at the 2nd nano second, you must be back on the path so
+you can only travel through 1 wall block.
+
+### Part b struggles
+* Didn't realize the cheat path can also go on '.'
+
+### Part b wrong ansers
+* 883350 - too low
+* 1104707 - too high
+
+## Performance
+```bash
+
+```
