8788b5ee1ac45810e6967b2c58da8f0046120eb9
Giovanni Go <dreamnid@dreamnid.com>
2024-12-22 21:09:17 -0600

    Part 2 of Day 22

--- 22/22.py
+++ 22/22.py
@@ -10,7 +10,7 @@ import os
 import pprint
 import re
 from time import time
-from typing import NamedTuple
+from typing import NamedTuple, Deque
 
 from humanize import intcomma
 import numpy as np
@@ -28,8 +28,8 @@ if __name__ == '__main__':
         from util import *
 
 INPUT_FILE='22-input.txt'
-INPUT_FILE='22a-example.txt'
-INPUT_FILE='22b-example.txt'
+# INPUT_FILE='22a-example.txt'
+# INPUT_FILE='22b-example.txt'
 
 input = [line for line in get_file_contents(INPUT_FILE)[0]]
 
@@ -57,27 +57,44 @@ def next_secret(num):
 new_secrets: list[int] = []
 prices: list[list[int]] = []
 price_changes: list[list[int]] = []
+
+overall_last_fource_changes_to_prize_total = defaultdict(int)
+
 with PrintTiming('a'):
     for line in input:
         secret = int(line)
         cur_prices = [secret % 10]
         cur_price_changes = []
         cur_highest_price = 0
+        last_four_changes_buffer = deque()
+        last_four_changes_to_price: dict[tuple, int] = dict()
         for i in range(2000):
             secret = next_secret(secret)
             cur_price = secret % 10
             if cur_price > cur_highest_price:
                 cur_highest_price = cur_price
+            last_four_changes_buffer.append(cur_price - cur_prices[-1])
+
+            if len(last_four_changes_buffer) == 4:
+                last_four_changes_key = tuple(last_four_changes_buffer)
+                # Only need to track the first time the 4 number sequence happens
+                if last_four_changes_key not in last_four_changes_to_price:
+                    last_four_changes_to_price[last_four_changes_key] = cur_price
+                last_four_changes_buffer.popleft()
+
             cur_price_changes.append(cur_price - cur_prices[-1])
             cur_prices.append(cur_price)
         new_secrets.append(secret)
         prices.append(cur_prices)
         price_changes.append(cur_price_changes)
 
+        for key, cur_price in last_four_changes_to_price.items():
+            overall_last_fource_changes_to_prize_total[key] += cur_price
+
 print('a', sum(new_secrets))
 
 
-pprint.pprint([[(prices[i][j], price_change) for j, price_change in enumerate(cur_price_changes)] for i, cur_price_changes in enumerate(price_changes)])
+# pprint.pprint([[(prices[i][j], price_change) for j, price_change in enumerate(cur_price_changes)] for i, cur_price_changes in enumerate(price_changes)])
 def find_change_idx(desired_seq: list[int], changes: list[int]):
     """Find price for the given change sequence"""
     seq_i = 0
@@ -91,10 +108,13 @@ def find_change_idx(desired_seq: list[int], changes: list[int]):
     return 0
 
 
-monkey_bought_price: list[int] = []
-for i in range(len(input)):
-    change_idx = find_change_idx([-1,-1,0,2], price_changes[i])
-    monkey_bought_price.append(prices[i][change_idx+1])
+# monkey_bought_price: list[int] = []
+# for i in range(len(input)):
+#     change_idx = find_change_idx([-1,-1,0,2], price_changes[i])
+#     monkey_bought_price.append(prices[i][change_idx+1])
+#
+# pprint.pprint(monkey_bought_price)
+# print('b:', sum(monkey_bought_price))
 
-pprint.pprint(monkey_bought_price)
-print('b:', sum(monkey_bought_price))
+print('b', max(overall_last_fource_changes_to_prize_total.values()))
+
---
+++ 22/README.md
@@ -0,0 +1,10 @@
+# Day 22
+
+## Performance
+```bash
+(advent2024) ~/w/a/22 ❯❯❯ time python 22.py                                                                                                                                                                                                    ⏎ main ✚ ✱ ◼
+timer a:  0:00:09.720607
+a 20068964552
+b 2246
+python 22.py  10.13s user 0.20s system 98% cpu 10.471 total
+```
